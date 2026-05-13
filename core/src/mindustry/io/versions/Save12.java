e663f7e034f5140d1305fb9c846846722cb7a117
EggleEgg <125359838+EggleEgg@users.noreply.github.com>
2026-05-13 17:27:40 +0200

    add temp files

---
+++ core/src/mindustry/io/versions/Save12.java
@@ -0,0 +1,11 @@
+package mindustry.io.versions;
+
+import mindustry.io.*;
+
+/** Adds persistent enabled flags for datapatches. */
+public class Save12 extends SaveVersion{
+
+    public Save12(){
+        super(12);
+    }
+}
